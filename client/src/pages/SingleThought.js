3f026a18a203a69223e9c09efb60b4aa18bd0493
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-09-11 22:37:47 -0700

    added the ability to post reactions to thought posts

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
+import Auth from '../utils/auth';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
@@ -32,7 +34,7 @@ const SingleThought = props => {
           <p>{thought.thoughtText}</p>
         </div>
       </div>
-
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
   );
